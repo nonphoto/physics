ec24013a451a27db3bab8d70d5b5a1c39e15d542
Jonas Luebbers <jluebbers@squarespace.com>
2017-07-24 15:48:57 -0400

    Add basic collision resolution function to entities

--- src/entity.js
+++ src/entity.js
@@ -2,11 +2,32 @@ import {vec2} from "gl-matrix"
 
 export default class Entity {
     constructor(x, y) {
-        this.p = vec2.fromValues(x, y)
-        this.v = vec2.create()
+        this.position = vec2.fromValues(x, y)
+        this.velocity = vec2.create()
+        this.mass = 1
+        this.inverseMass = 1 / this.mass
     }
 
-    draw(dt) {
+    static resolveCollsion(manifold) {
+        const {a, b, normal, penetration} = manifold
 
+        const relativeVelocity = vec2.create()
+        vec2.subtract(relativeVelocity, a.velocity, b.velocity)
+
+        const normalVelocity = vec2.dot(relativeVelocity, normal)
+
+        if (normalVelocity > 0) {
+            return
+        }
+
+        const restitution = min(a.restitution, b.restitution)
+
+        const impulseMagnitude = (-1 - restitution) * normalVelocity / (a.inverseMass + b.inverseMass)
+
+        const impulse = vec2.create()
+        vec2.scale(impulse, normal, impulseMagnitude)
+
+        a.velocity -= a.inverseMass * impulse
+        b.velocity += b.inverseMass * impulse
     }
 }
